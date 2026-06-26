b6ab2fe4e2255a1479553017cc8991f89e7a09c5
msunzulu-dev <msunzulu@gmail.com>
2026-06-26 21:30:06 +0200

    Add files via upload

---
+++ Create dbo.AspNetUsers.sql
@@ -0,0 +1,46 @@
+﻿USE [aspnet-TrafficManagementSystem-20240828113625]
+GO
+
+/****** Object: Table [dbo].[AspNetUsers] Script Date: 10/12/2024 7:10:02 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[AspNetUsers] (
+    [Id]                   NVARCHAR (128) NOT NULL,
+    [Account]              FLOAT (53)     NOT NULL,
+    [Email]                NVARCHAR (256) NULL,
+    [EmailConfirmed]       BIT            NOT NULL,
+    [PasswordHash]         NVARCHAR (MAX) NULL,
+    [SecurityStamp]        NVARCHAR (MAX) NULL,
+    [PhoneNumber]          NVARCHAR (MAX) NULL,
+    [PhoneNumberConfirmed] BIT            NOT NULL,
+    [TwoFactorEnabled]     BIT            NOT NULL,
+    [LockoutEndDateUtc]    DATETIME       NULL,
+    [LockoutEnabled]       BIT            NOT NULL,
+    [AccessFailedCount]    INT            NOT NULL,
+    [UserName]             NVARCHAR (256) NOT NULL
+);
+
+
+GO
+CREATE UNIQUE NONCLUSTERED INDEX [UserNameIndex]
+    ON [dbo].[AspNetUsers]([UserName] ASC);
+
+
+GO
+ALTER TABLE [dbo].[AspNetUsers]
+    ADD CONSTRAINT [PK_dbo.AspNetUsers] PRIMARY KEY CLUSTERED ([Id] ASC);
+
+
+			SELECT *
+FROM [dbo].[AspNetUsers];
+GO
+
+DELETE FROM [dbo].[StudentTrackings];
+GO
+
+DELETE FROM [dbo].[AspNetUsers]
+WHERE Email = 'officer@gmail.com';
---
+++ Create dbo.MetroOfficers.sql
@@ -0,0 +1,42 @@
+﻿USE [aspnet-TrafficManagementSystem-20240828113625]
+GO
+
+/****** Object: Table [dbo].[MetroOfficers] Script Date: 10/12/2024 7:08:14 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[MetroOfficers] (
+    [MetroOfficerId]     INT            IDENTITY (1, 1) NOT NULL,
+    [UserId]             NVARCHAR (MAX) NULL,
+    [Email]              NVARCHAR (MAX) NOT NULL,
+    [FirstName]          NVARCHAR (MAX) NOT NULL,
+    [LastName]           NVARCHAR (MAX) NOT NULL,
+    [Phone]              NVARCHAR (MAX) NOT NULL,
+    [Picture]            NVARCHAR (MAX) NOT NULL,
+    [ApplicationUser_Id] NVARCHAR (128) NULL
+);
+
+
+GO
+CREATE NONCLUSTERED INDEX [IX_ApplicationUser_Id]
+    ON [dbo].[MetroOfficers]([ApplicationUser_Id] ASC);
+
+
+GO
+ALTER TABLE [dbo].[MetroOfficers]
+    ADD CONSTRAINT [PK_dbo.MetroOfficers] PRIMARY KEY CLUSTERED ([MetroOfficerId] ASC);
+
+
+GO
+ALTER TABLE [dbo].[MetroOfficers]
+    ADD CONSTRAINT [FK_dbo.MetroOfficers_dbo.AspNetUsers_ApplicationUser_Id] FOREIGN KEY ([ApplicationUser_Id]) REFERENCES [dbo].[AspNetUsers] ([Id]);
+
+			SELECT *
+FROM [dbo].[MetroOfficers];
+GO
+
+DELETE FROM [dbo].[MetroOfficers];
+GO
